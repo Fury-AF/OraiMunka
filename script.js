ed75209e55c78cfff343ac5339230724e4fdfc10
dbalazs95 <dbalazs9511@gmail.com>
2022-05-12 18:12:41 +0200

    a feldolgoz szuloeleme object HTMLElement valamiér

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-array.forEach(element => {
-    
-});
+window.addEventListener('load', init)
+function $(elem) {
+  return document.querySelectorAll(elem)
+}
+
+const cikkek = [];
+
+function init() {
+    let cikk = "cikkek.json";
+    fetch(cikk)
+        .then((res) => res.json())
+        .then((data) => {
+            //console.log(data.cikk)
+            data.cikk.forEach((elem) => {
+                cikkek.push(elem);
+            });
+        
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+
+    console.log(cikkek)
+    feldolgoz(cikkek)
+}
+function feldolgoz(cikkek) {
+    const szuloelem=document.querySelector("article");
+    Object.keys(cikkek).forEach(function (cikk){
+        szuloelem.innerHTML += "<br>";
+        for (const key in cikkek){
+            szuloelem.innerHTML += "<div><span>"+cikkek[key]+ "</span></div>";
+        }
+    });
+    console.log(szuloelem)
+  $('article')[0].innerHTML = szuloelem
+}
+
